0821ce2309ba07a7684416b242311f1aa9ed2dc8
Manasés Jesús <contact@manasesjesus.com>
2019-02-25 10:47:25 +0100

    adding hello world base code

--- hello-world-microservice/src/main/java/c8y/example/App.java
+++ hello-world-microservice/src/main/java/c8y/example/App.java
@@ -1,13 +1,20 @@
 package c8y.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.cumulocity.microservice.autoconfigure.MicroserviceApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@MicroserviceApplication
+@RestController
+public class App {
+    public static void main (String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping("hello")
+    public String greeting (@RequestParam(value = "who", defaultValue = "world") String who) {
+        return "hello " + who + "!";
     }
 }
