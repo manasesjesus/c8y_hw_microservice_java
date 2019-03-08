c878ac7c1d6f2d39d51848c144936dd4e8817440
Manasés Jesús <contact@manasesjesus.com>
2019-03-08 17:50:09 +0100

    checkpoint - code to get events

--- hello-world-microservice/src/main/java/c8y/example/App.java
+++ hello-world-microservice/src/main/java/c8y/example/App.java
@@ -2,8 +2,19 @@ package c8y.example;
 
 import java.util.HashMap;
 import java.util.Map;
+// import java.util.concurrent.Callable;
 
 import com.cumulocity.microservice.autoconfigure.MicroserviceApplication;
+/*import com.cumulocity.microservice.context.ContextService;
+import com.cumulocity.microservice.context.credentials.MicroserviceCredentials;
+import com.cumulocity.microservice.context.inject.TenantScope;
+import com.cumulocity.microservice.subscription.model.core.PlatformProperties;
+import com.cumulocity.sdk.client.Platform;
+import com.cumulocity.sdk.client.SDKException;
+import com.cumulocity.sdk.client.event.EventApi;
+import com.cumulocity.sdk.client.event.PagedEventCollectionRepresentation;
+
+import org.springframework.beans.factory.annotation.Autowired;*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +23,29 @@ import org.springframework.web.bind.annotation.RestController;
 @MicroserviceApplication
 @RestController
 public class App {
+
+/*    @Autowired
+    private PlatformProperties platformProperties;
+    @Autowired
+    private ContextService<MicroserviceCredentials> contextService;
+    @Autowired
+    private EventApi eventApi;
+
+    @TenantScope
+    public EventApi eventApi(Platform platform) throws SDKException {
+        return platform.getEventApi();
+    }  
+
+    public PagedEventCollectionRepresentation get10Events() {
+        return contextService.callWithinContext(
+                (MicroserviceCredentials) platformProperties.getMicroserviceBoostrapUser()
+                , new Callable<PagedEventCollectionRepresentation>(){
+            public PagedEventCollectionRepresentation call(){
+                return eventApi.getEvents().get(10);
+            }
+        });
+    } */
+
     public static void main (String[] args) {
         SpringApplication.run(App.class, args);
     }
@@ -21,6 +55,9 @@ public class App {
      */
     @RequestMapping("hello")
     public String greeting (@RequestParam(value = "name", defaultValue = "World") String you) {
+
+//        System.out.println(get10Events());
+
         return "Hello " + you + "!";
     }
 
