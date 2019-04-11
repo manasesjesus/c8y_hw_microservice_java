5e541460f6172e70d0c062a2be7764e99cc06c65
Manasés Jesús <contact@manasesjesus.com>
2019-04-11 17:53:52 +0200

    ignoring unknown JSON properties

--- hello-world-microservice/src/main/java/c8y/example/Location.java
+++ hello-world-microservice/src/main/java/c8y/example/Location.java
@@ -2,6 +2,7 @@ package c8y.example;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Location {
     
     private String type;
