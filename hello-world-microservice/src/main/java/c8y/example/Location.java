db74b95b461ff47dd7b8a70d85d60fe3d4d718e8
Manasés Jesús <contact@manasesjesus.com>
2019-04-11 17:51:47 +0200

    adding Location class

---
+++ hello-world-microservice/src/main/java/c8y/example/Location.java
@@ -0,0 +1,24 @@
+package c8y.example;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+public class Location {
+    
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+               "type : '" + type + "'" +
+               "}";
+    }
+
+}
