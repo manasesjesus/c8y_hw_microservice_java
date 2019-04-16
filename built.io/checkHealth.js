6264727a5c394b1d397cb42b6b0b48e15023c1d0
Manasés Jesús <contact@manasesjesus.com>
2019-04-16 14:12:48 +0200

    improving the code

--- built.io/checkHealth.js
+++ built.io/checkHealth.js
@@ -3,15 +3,15 @@ const request = require("request");
 const credentials = $config.params.username + ":" + $config.params.password;
 const ms_endpoint = $config.params.server + "/service/" + $config.params.microservice + "/health";
 
-const headerOption = {
+const options = {
     "url" : ms_endpoint,
     "headers" : {
-      "Authorization" : "Basic " + new Buffer(credentials).toString("base64"),
+      "Authorization" : "Basic " + Buffer.from(credentials).toString("base64"),
       "Accept" : "application/json"
     }
 };
 
-request(headerOption, function (error, response, body) {
+request(options, function (error, response, body) {
     
     let status = JSON.parse(body).status;
 
