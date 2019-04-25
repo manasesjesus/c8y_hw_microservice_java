a7ebcfee1483209cc9c6484a85e13b267c9089b2
Manasés Jesús <contact@manasesjesus.com>
2019-04-25 17:27:28 +0200

    adding Java 12 microservice project

---
+++ my-first-microservice/src/main/java/c8y/example/Credentials.java
@@ -0,0 +1,26 @@
+package c8y.example;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
+
+public class Credentials {
+    public static String USERNAME;
+    public static String PASSWD;
+    public static String URL;
+    
+    public static String IPSTACK_KEY;
+
+    public static void loadCredentials () throws IOException {
+        Properties props   = new Properties();
+        Reader credentials = new FileReader("/etc/my-first-microservice/credentials.properties");
+
+        props.load(credentials);
+        USERNAME = props.getProperty("c8y.tenant.id") + "/" + props.getProperty("c8y.username");
+        PASSWD   = props.getProperty("c8y.passwd");
+        URL      = props.getProperty("c8y.url");
+        
+        IPSTACK_KEY = props.getProperty("ipstack.key");
+    }
+}
